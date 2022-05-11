a2b24fbfd255c28a918e2b5264c41a71f40cb115
Bor Bálint <br.blnt10@gmail.com>
2022-05-11 15:37:35 +0200

    Fixes

--- contacts/data/src/main/java/hu/nye/progkor/populator/impl/DatabasePopulatorImpl.java
+++ contacts/data/src/main/java/hu/nye/progkor/populator/impl/DatabasePopulatorImpl.java
@@ -42,10 +42,13 @@ public class DatabasePopulatorImpl implements DatabasePopulator {
     @Override
     public void populateDatabase() {
         log.info("Create stock contacts and users to database.");
+        saveUsers();
         repository.saveAll(CONTACTS);
     }
 
     private void saveUsers() {
+        if(userRepository.findAll().contains(admin))
+                return;
         userRepository.save(admin);
     }
 }
