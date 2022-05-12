681ebc3fbe870060411da59257cfbc5722c9e2df
Bor Bálint <br.blnt10@gmail.com>
2022-05-12 08:58:58 +0200

    Checkstyle
    - Corrections

--- contacts/data/src/main/java/hu/nye/progkor/model/User.java
+++ contacts/data/src/main/java/hu/nye/progkor/model/User.java
@@ -1,8 +1,19 @@
 package hu.nye.progkor.model;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.GenerationType;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import lombok.Data;
 
 /**
  * Represents a user in the database. Contains the users' id, name.
--- contacts/service/src/main/java/hu/nye/progkor/export/ContactsToPDF.java
+++ contacts/service/src/main/java/hu/nye/progkor/export/ContactsToPDF.java
@@ -1,12 +1,18 @@
 package hu.nye.progkor.export;
 
-import java.awt.*;
+
+import java.awt.Color;
 import java.io.IOException;
 import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 
-import com.lowagie.text.*;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Document;
 import com.lowagie.text.Font;
+import com.lowagie.text.FontFactory;
+import com.lowagie.text.PageSize;
 import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
